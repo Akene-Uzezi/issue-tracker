e0e8da3b789bf941544a7d53b6bf276290332444
Akene Uzezi <xtremejoexj@gmail.com>
2026-04-25 22:06:16 +0100

    added the prisma files

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// prisma.config.ts
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    // This is where Migrate gets its connection string now
+    url: process.env["DATABASE_URL"],
+  },
+});
